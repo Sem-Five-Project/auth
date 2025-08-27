e25f8982439d58e0bf086c793a86efd6ec656362
krishmal gamage <krishmalgamage20021027@gmail.com>
2025-08-27 17:04:20 +0530

    Update LoginRequest.java

--- src/main/java/com/edu/tutor_platform/user/dto/LoginRequest.java
+++ src/main/java/com/edu/tutor_platform/user/dto/LoginRequest.java
@@ -1,10 +1,46 @@
-package com.edu.tutor_platform.user.dto;
+// package com.edu.tutor_platform.user.dto;
 
-import lombok.Data;
+// import lombok.Data;
 
-@Data
+// @Data
+// public class LoginRequest {
+//     private String email;
+//     private String password;
+
+// }
+package com.authsystem.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
 public class LoginRequest {
-    private String email;
+    
+    @NotBlank(message = "Username or email is required")
+    private String usernameOrEmail;
+    
+    @NotBlank(message = "Password is required")
     private String password;
-
+    
+    public LoginRequest() {}
+    
+    public LoginRequest(String usernameOrEmail, String password) {
+        this.usernameOrEmail = usernameOrEmail;
+        this.password = password;
+    }
+    
+    public String getUsernameOrEmail() {
+        return usernameOrEmail;
+    }
+    
+    public void setUsernameOrEmail(String usernameOrEmail) {
+        this.usernameOrEmail = usernameOrEmail;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
